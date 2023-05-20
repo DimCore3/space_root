109b829120ae79a300f1f654cd2abfa0673c2536
DimCore3 <3dmi3y@gmail.com>
2023-05-20 04:01:41 +0400

    добавил гит в хэддер

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,6 +1,7 @@
 const Header = () => {
     return ( 
         <header>
+            <a href="https://github.com/DimCore3/">GitHub</a>
             <a href="https://www.linkedin.com/in/dimcore3/">Linkedin</a>
             <a href="https://t.me/DimCor3">Telegram</a>
         </header>
