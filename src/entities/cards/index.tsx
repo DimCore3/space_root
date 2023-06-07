feda0dbd314d2a082cdf36ac8f2ac5c2a2bbf64f
DimCore3 <3dmi3y@gmail.com>
2023-06-07 14:19:38 +0400

    Поправил косяк с ховером, теперь иконки не пляшут

--- src/entities/cards/index.tsx
+++ src/entities/cards/index.tsx
@@ -5,13 +5,15 @@ const Cards = ({ data }: Props) => {
     return (
         <div className="cards">
             {data.map((e, index) => (
-                <div className="content_portfolio" key={"content_portfolio_" + index}>
-                    <a href={e.link.includes('http') ? e.link : './portfolios/' + e.link} className="sidebar_url">
-                        <h3>{e.name}</h3>
-                        <img src={require('./assets/img/' + e.img)} alt={e.alt ?? ''} />
-                    </a>
-                    {e.description && <p>{e.description}</p>}
-                    {e.gitLink && <a href={e.gitLink}>Git project link</a>}
+                <div className="content_portfolio_background">
+                    <div className="content_portfolio" key={"content_portfolio_" + index}>
+                        <a href={e.link.includes('http') ? e.link : './portfolios/' + e.link} className="sidebar_url">
+                            <h3>{e.name}</h3>
+                            <img src={require('./assets/img/' + e.img)} alt={e.alt ?? ''} />
+                        </a>
+                        {e.description && <p>{e.description}</p>}
+                        {e.gitLink && <a href={e.gitLink}>Git project link</a>}
+                    </div>
                 </div>
             ))}
         </div>
